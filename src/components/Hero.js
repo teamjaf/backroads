9305526df06fa3662fb1e5e85b85e42b14960e8d
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-02-11 00:48:21 +0600

    component separated

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section class="hero" id="home">
+      <div class="hero-banner">
+        <h1>continue exploring</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.
+        </p>
+        <a href="#tours" class="btn hero-btn">explore tours</a>
+      </div>
+    </section>
+  )
+}
